0020329501b01fac759f1a6e607099c1f34ec268
Ivan Rodrigo <ivan.jara.a@tecsup.edu.pe>
2026-08-28 17:08:48 -0500

    Implementa cálculos de costos y créditos

--- Semana02/app/src/main/java/com/jara/uni/Matricula.kt
+++ Semana02/app/src/main/java/com/jara/uni/Matricula.kt
@@ -22,4 +22,32 @@ fun main() {
         print("Créditos del curso: ")
         creditosCursos.add(readLine()!!.toInt())
     }
+
+    // ---------- 2. CÁLCULOS ----------
+    val costosCursos = mutableListOf<Double>()
+    var totalCreditos = 0
+    var totalPagar = 0.0
+
+    for (i in 0 until cantidadCursos) {
+        val costo = creditosCursos[i] * valorCredito
+        costosCursos.add(costo)
+        totalCreditos += creditosCursos[i]
+        totalPagar += costo
+    }
+
+    val cargaAcademica: String
+    if (totalCreditos <= 12) {
+        cargaAcademica = "Malla Regular"
+    } else if (totalCreditos <= 18) {
+        cargaAcademica = "Carga Máxima"
+    } else {
+        cargaAcademica = "Requiere autorización RENU"
+    }
+
+    val formaPago: String
+    if (totalPagar > 2500) {
+        formaPago = "3 cuotas"
+    } else {
+        formaPago = "2 cuotas"
+    }
 }
