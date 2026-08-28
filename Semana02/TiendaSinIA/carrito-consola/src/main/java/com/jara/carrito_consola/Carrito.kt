782ccc8214a7b94b565f6f4a82369f21a6ff098a
Ivan <ivan.jara.a@tecsup.edu.pe>
2026-08-27 23:51:37 -0500

    Semana02: agrega proyecto TiendaSinIA

---
+++ Semana02/TiendaSinIA/carrito-consola/src/main/java/com/jara/carrito_consola/Carrito.kt
@@ -0,0 +1,7 @@
+package com.jara.carrito_consola
+
+fun main() {
+    println("=========================================")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("=========================================")
+}
