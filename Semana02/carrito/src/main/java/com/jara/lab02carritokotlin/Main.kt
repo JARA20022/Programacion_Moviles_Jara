e6a291d17751a58e40ae74864c6f21af150c409d
Ivan <ivan.jara.a@tecsup.edu.pe>
2026-08-27 23:04:57 -0500

    Agregue texto  semana02

--- Semana02/carrito/src/main/java/com/jara/lab02carritokotlin/Main.kt
+++ Semana02/carrito/src/main/java/com/jara/lab02carritokotlin/Main.kt
@@ -2,7 +2,7 @@ package com.jara.lab02carritokotlin
 
 fun main() {
     println("=========================================")
-    println("   CARRITO DE COMPRAS - TIENDA  ")
+    println("   CARRITO DE COMPRAS - TIENDA  TECSUP")
     println("=========================================")
     println()
 
