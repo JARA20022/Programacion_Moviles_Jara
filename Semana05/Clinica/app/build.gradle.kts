875a01325d5c12226e1cca71b43879ae89e1d452
Ivan <ivan.jara.a@tecsup.edu.pe>
2026-09-23 00:42:47 -0500

    Corrige compileSdk para las dependencias de AndroidX
    Actualiza compileSdk de 36.1 a 37 para satisfacer los requisitos de AAR metadata de Core y Lifecycle. Conserva los valores existentes de minSdk y targetSdk.

--- Semana05/Clinica/app/build.gradle.kts
+++ Semana05/Clinica/app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 android {
     namespace = "com.jara.clinicasalud"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
